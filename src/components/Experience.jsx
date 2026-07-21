078d11d1478a6447d041c4a3c08ec0ce1d007f3d
Rakshitha HN <rakshug73@gmail.com>
2026-07-21 14:50:31 +0530

    Add Experience component with internship details

---
+++ src/components/Experience.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "./Experience.css";
+
+
+function Experience(){
+
+return(
+
+<section
+id="experience"
+className="experience"
+data-aos="fade-up"
+>
+
+<div className="experience-container">
+
+<h2 className="section-title">
+Experience
+</h2>
+
+
+<div className="experience-card">
+
+
+<h3>
+Frontend Developer Intern
+</h3>
+
+
+<h4>
+Ideonix Solutions, Mysuru
+</h4>
+
+
+<p className="experience-duration">
+Nov 2023 - Dec 2023
+</p>
+
+
+<ul>
+
+<li>
+Developed responsive web interfaces using HTML, CSS and JavaScript.
+</li>
+
+<li>
+Worked on frontend components and improved user interface experience.
+</li>
+
+<li>
+Debugged frontend issues and enhanced application performance.
+</li>
+
+<li>
+Collaborated with team members to build efficient web solutions.
+</li>
+
+</ul>
+
+
+</div>
+
+
+</div>
+
+</section>
+
+);
+
+}
+
+
+export default Experience;
