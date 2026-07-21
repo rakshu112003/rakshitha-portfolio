b0efd2cd5e8b0b2a9ac4ef3232bfc9d36a7a7bd2
Rakshitha HN <rakshug73@gmail.com>
2026-07-21 14:58:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 import Education from "./components/Education";
 import Resume from "./components/Resume";
 import Contact from "./components/Contact";
@@ -30,6 +31,7 @@ function App() {
       <About />
       <Skills />
       <Projects />
+      <Experience />
       <Education />
       <Resume />
       <Contact />
